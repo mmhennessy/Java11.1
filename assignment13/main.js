426aa2bbfb50a354a27ff954bb8ebb6a3cf9736c
mmhennessy <mhennessy@mcad.edu>
2015-11-22 23:31:03 -0600

    assignment 13
    commit

---
+++ assignment13/main.js
@@ -0,0 +1,21 @@
+  document.getElementById("a");
+        $("a").click(function() {
+            $("a").css("color", "orange");
+    });
+      
+      $(document).ready(function() {
+        $("h1").text("RESTARAUNT MENU-CLICK FOR LARGER FONT");
+ 
+
+      $(function() {
+        
+         $('a').css('margin-right', '25px');
+
+         $('a').css({textAlign: 'center'});
+
+         $('h1').css('background', 'green');
+
+         $('h2, h3').css('color', 'blue');
+
+      })
+    });
